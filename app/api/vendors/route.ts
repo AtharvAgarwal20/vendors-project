c3592388c84156ad2e7674e2db672b45caeddb6b
Atharv Agarwal <atharv20agarwal@gmail.com>
2025-07-04 01:02:50 +0530

    fix: error handling on POST api

--- app/api/vendors/route.ts
+++ app/api/vendors/route.ts
@@ -1,6 +1,6 @@
 import { vendorSchema } from "@/app/utils/schemas";
 import { auth } from "@/auth";
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 import { ZodError } from "zod/v4";
 
@@ -48,5 +48,20 @@ export async function POST(req: Request) {
       console.log(error);
       return NextResponse.json({ error: "Invalid Form Data" }, { status: 400 });
     }
+
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === "P2002"
+    ) {
+      return NextResponse.json(
+        { error: "Vendor name already in use" },
+        { status: 409 }
+      );
+    }
+
+    return NextResponse.json(
+      { error: "An unexpected error occured" },
+      { status: 500 }
+    );
   }
 }
